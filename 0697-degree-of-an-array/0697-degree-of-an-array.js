f194813e16475708911ec3a0512791ff157ed641
David Tan <dtannyc1@gmail.com>
2023-07-25 11:35:49 -0400

    Time: 92 ms (45.34%), Space: 48.2 MB (28.34%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+    let counter = ({});
+    let sols = {maxCount: 0, maxKey: []}
+    for (let ii = 0; ii < nums.length; ii++) {
+        if (!counter[nums[ii]]) {
+            counter[nums[ii]] = {count: 1, start: ii, end: ii}
+            if (sols.maxCount === 0) {
+                sols.maxCount = 1;
+                sols.maxKey = [nums[ii]];
+            }
+        } else {
+            counter[nums[ii]].count += 1;
+            counter[nums[ii]].end = ii;
+
+            if (counter[nums[ii]].count > sols.maxCount) {
+                sols.maxCount = counter[nums[ii]].count;
+                sols.maxKey = [nums[ii]];
+            } else if (counter[nums[ii]].count === sols.maxCount) {
+                // more than one option
+                sols.maxKey.push(nums[ii])
+            }
+        }
+    }
+
+    let ans = [];
+    for (let ii = 0; ii < sols.maxKey.length; ii++) {
+        let key = sols.maxKey[ii];
+        ans.push(counter[key].end-counter[key].start+1);
+    }
+    
+    return Math.min(...ans)
+};
