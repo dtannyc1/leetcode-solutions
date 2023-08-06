d0b6c6476a84b64cf2adb658ec6a48b0af17fd0d
David Tan <dtannyc1@gmail.com>
2023-08-06 14:44:15 -0400

    Time: 108 ms (96.91%), Space: 48.7 MB (98.31%) - LeetHub

--- 1708-design-parking-system/1708-design-parking-system.js
+++ 1708-design-parking-system/1708-design-parking-system.js
@@ -4,9 +4,7 @@
  * @param {number} small
  */
 var ParkingSystem = function(big, medium, small) {
-    this.bigLim = big;
-    this.medLim = medium; 
-    this.smlLim = small;
+    this.lims = [big,medium,small];
     this.cars = [0,0,0];
 };
 
@@ -15,27 +13,11 @@ var ParkingSystem = function(big, medium, small) {
  * @return {boolean}
  */
 ParkingSystem.prototype.addCar = function(carType) {
-    if (carType === 1) {
-        if (this.cars[0] < this.bigLim){
-            this.cars[0] += 1;
-            return true;
-        } else {
-            return false;
-        }
-    } else if (carType === 2){
-        if (this.cars[1] < this.medLim) {
-            this.cars[1] += 1;
-            return true;
-        } else {
-            return false;
-        }
-    } else if (carType === 3) {
-        if (this.cars[2] < this.smlLim) {
-            this.cars[2] += 1;
-            return true;
-        } else {
-            return false;
-        }
+    if (this.cars[carType - 1] < this.lims[carType - 1]) {
+        this.cars[carType - 1] += 1;
+        return true;
+    } else {
+        return false;
     }
 };
 
