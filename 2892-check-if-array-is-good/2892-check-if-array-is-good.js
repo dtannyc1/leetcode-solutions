4edcd731b8cca86de4d573a78c21896eed4a0b29
David Tan <dtannyc1@gmail.com>
2023-08-08 12:07:25 -0400

    Time: 58 ms (85.05%), Space: 45 MB (10.05%) - LeetHub

--- 2892-check-if-array-is-good/2892-check-if-array-is-good.js
+++ 2892-check-if-array-is-good/2892-check-if-array-is-good.js
@@ -3,17 +3,16 @@
  * @return {boolean}
  */
 var isGood = function(nums) {
-    let count = ({});
-    let n = nums.length - 1;
+    let count = new Array(nums.length - 1).fill(0);
     for (let ii = 0; ii < nums.length; ii++){
         if (!count[nums[ii]]) {
-            if (nums[ii] <= n && nums[ii] > 0) {
+            if (nums[ii] <= nums.length - 1 && nums[ii] > 0) {
                 count[nums[ii]] = 1;
             } else {
                 return false;
             }
         } else {
-            if (nums[ii] === n && count[nums[ii]] === 1){
+            if (nums[ii] === nums.length - 1 && count[nums[ii]] === 1){
                 count[nums[ii]] += 1;
             } else {
                 return false;
