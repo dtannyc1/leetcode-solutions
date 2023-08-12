3e0e6a73ac6c8966d7952e647be5e9e87f648ec6
David Tan <dtannyc1@gmail.com>
2023-08-12 18:58:36 -0400

    Time: 55 ms (94.16%), Space: 44.4 MB (21.57%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.js
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.js
@@ -4,8 +4,22 @@
  */
 var isFascinating = function(n) {
     let nums = ({});
-    let chars = (n.toString() + (2*n).toString() + (3*n).toString()).split("");
-    for (let char of chars){
+    // let chars = (n.toString() + (2*n).toString() + (3*n).toString()).split("");
+    for (let char of n.toString().split("")){
+        if (char === '0' || nums[char]) {
+            return false;
+        } else {
+            nums[char] = true;
+        }
+    }
+    for (let char of (2*n).toString().split("")){
+        if (char === '0' || nums[char]) {
+            return false;
+        } else {
+            nums[char] = true;
+        }
+    }
+    for (let char of (3*n).toString().split("")){
         if (char === '0' || nums[char]) {
             return false;
         } else {
