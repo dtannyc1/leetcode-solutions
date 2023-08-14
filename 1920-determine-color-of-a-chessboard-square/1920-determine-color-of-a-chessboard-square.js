ae9a7a1207ec02388a7d4d69ca34079586b9a1ac
David Tan <dtannyc1@gmail.com>
2023-08-14 15:22:58 -0400

    Time: 58 ms (41.48%), Space: 41.8 MB (42.61%) - LeetHub

--- 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.js
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var squareIsWhite = function(coordinates) {
-    return ((coordinates.charCodeAt(0) - 97) + (coordinates.charCodeAt(1))) % 2 === 0;
+    return ((coordinates.charCodeAt(0)) + (coordinates.charCodeAt(1))) % 2 === 1;
 };
