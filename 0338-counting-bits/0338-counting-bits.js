63cb209c3a6c5756f0cecb1f0e622f842d73780b
David Tan <dtannyc1@gmail.com>
2023-08-16 16:48:08 -0400

    Time: 75 ms (79.52%), Space: 49 MB (55.29%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -4,29 +4,34 @@
  */
 var countBits = function(n) {
     if (n === 0) return [0];
-    if (n === 1) return [0,1];
-    if (n === 2) return [0,1,1];
-    let sol = [0,1,1];
-    let list = [1,2];
-    let ind = 1;
-    // let nextPow = 2;
-    // let i = 2;
-    // let even = true;
 
-    while (sol.length <= n) {
-        // i++;
-        // even = !even;
-        // if (i === 2 ** nextPow){
+    // let sol = [0];
+    // let list = [1];
+    // let ind = 0;
+
+    // while (sol.length <= n) {
+    //     sol.push(list[ind]);
+    //     ind++;
+    //     if (ind === list.length){
+    //         ind = 0;
+    //         list = list.concat(list.map(val => val+1));
+    //     }
+    // }
+
+    let sol = [0,1];
+    let list = [1,2];
+    let ind = 0;
+
+    while (sol.length + list.length <= n) {
+        // sol.push(list[ind]);
+        // ind++;
+        // if (ind === list.length){
         //     ind = 0;
-        //     nextPow++;
         //     list = list.concat(list.map(val => val+1));
         // }
-        sol.push(list[ind]);
-        ind++;
-        if (ind === list.length){
-            ind = 0;
-            list = list.concat(list.map(val => val+1));
-        }
+        sol = sol.concat(list);
+        list = list.concat(list.map(val => val+1));
     }
+    sol = sol.concat(list.slice(0, n + 1 - list.length))
     return sol;
 };
