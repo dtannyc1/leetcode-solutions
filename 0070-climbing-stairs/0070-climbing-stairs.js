6794ace67559a889e108a4562f30ec15978f7978
David Tan <dtannyc1@gmail.com>
2023-08-25 17:38:20 -0400

    Time: 50 ms (74.46%), Space: 41.5 MB (81.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,14 +3,28 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let table = {1: 1, 2: 2};
+    // let table = {1: 1, 2: 2};
 
-    let helper = (m) => {
-        if (table[m]) return table[m];
-        else {
-            table[m] = helper(m-1) + helper(m-2);
-            return table[m];
-        }
+    // let helper = (m) => {
+    //     if (table[m]) return table[m];
+    //     else {
+    //         table[m] = helper(m-1) + helper(m-2);
+    //         return table[m];
+    //     }
+    // }
+    // return helper(n);
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+
+    let ii = 2;
+    let prev = 1;
+    let cur = 2;
+
+    while (ii < n){
+        let tmp = cur;
+        cur += prev;
+        prev = tmp;
+        ii++;
     }
-    return helper(n);
+    return cur;
 };
