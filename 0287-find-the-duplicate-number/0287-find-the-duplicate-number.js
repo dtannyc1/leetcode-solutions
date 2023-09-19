96d3ecbfeaee5329bbcbb59eb83685f644d6a634
David Tan <dtannyc1@gmail.com>
2023-09-19 10:45:05 -0400

    Time: 64 ms (92.98%), Space: 53.2 MB (45.8%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,10 +3,15 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let data = new Array(nums.length);
+    let data = new Array(nums.length).fill(0);
 
     for (let num of nums){
         if (data[num]) return num;
         data[num] = true;
     }
+
+    // // Pure math approach: sum - n(n+1)/2 -> only works for one repeat
+    // let sum = nums.reduce((part, num) => part + num, 0);
+    // let n = nums.length-1;
+    // return sum - n*(n+1)/2;
 };
