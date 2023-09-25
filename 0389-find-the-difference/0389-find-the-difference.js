61a69e101e6f38a2ea6b8919852d01b31a7f16e7
David Tan <dtannyc1@gmail.com>
2023-09-25 10:38:17 -0400

    Time: 67 ms (26.25%), Space: 44.4 MB (43.55%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -6,14 +6,22 @@
 var findTheDifference = function(s, t) {
     let dict = ({});
     for (let ii = 0; ii < s.length; ii++){
-        if (!dict[s.charAt(ii)]) dict[s.charAt(ii)] = [ii];
-        else dict[s.charAt(ii)].push(ii);
+        if (!dict[s.charAt(ii)]) dict[s.charAt(ii)] = 1;
+        else dict[s.charAt(ii)]++;
     }
 
     for (let jj = 0; jj < t.length; jj++){
-        if (!dict[t.charAt(jj)] || dict[t.charAt(jj)].length === 0) return t.charAt(jj);
-        dict[t.charAt(jj)].pop();
+        if (!dict[t.charAt(jj)]) return t.charAt(jj);
+        dict[t.charAt(jj)]--;
     }
 
     return null
+    // let sArr = s.split("").sort();
+    // let tArr = t.split("").sort();
+    // for (let ii = 0; ii < sArr.length; ii++){
+    //     if (sArr[ii] != tArr[ii]){
+    //         return tArr[ii];
+    //     }
+    // }
+    // return tArr[tArr.length-1];
 };
