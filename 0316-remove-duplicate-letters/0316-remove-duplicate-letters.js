67396da7fca82fe710eeae9af8cab30f6d01a84e
David Tan <dtannyc1@gmail.com>
2023-09-26 13:22:13 -0400

    Time: 54 ms (88.51%), Space: 43.7 MB (31.08%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicateLetters = function(s) {
+    // go in reverse, put chars into arr
+    // if char already in arr, check if prev char is smaller
+        // if it is, dont add
+        // if it isnt, remove previous from arr and add to end
+    // return arr.reverse.join
+
+    
+    let outputArr = [];
+    let chars = new Set();
+    let lastIdx = {};
+    for (let ii = 0; ii < s.length; ii++){
+        lastIdx[s.charAt(ii)] = ii;
+    }
+
+    for (let ii = 0; ii < s.length; ii++){
+        if (!chars.has(s.charAt(ii))){
+            while (outputArr.length && s.charAt(ii) < outputArr[outputArr.length-1] && ii < lastIdx[outputArr[outputArr.length-1]]){
+                chars.delete(outputArr.pop());
+            }
+
+            chars.add(s.charAt(ii));
+            outputArr.push(s.charAt(ii));
+        }
+    }
+    return outputArr.join("");
+};
