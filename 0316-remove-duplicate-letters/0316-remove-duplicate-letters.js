48110b714fcc8d307aa9463f81ba7fbf00351cfa
David Tan <dtannyc1@gmail.com>
2023-09-26 14:36:48 -0400

    Time: 67 ms (27.03%), Space: 43.3 MB (36.49%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.js
@@ -14,17 +14,17 @@ var removeDuplicateLetters = function(s) {
     let chars = new Set();
     let lastIdx = {};
     for (let ii = 0; ii < s.length; ii++){
-        lastIdx[s.charAt(ii)] = ii;
+        lastIdx[s[ii]] = ii;
     }
 
     for (let ii = 0; ii < s.length; ii++){
-        if (!chars.has(s.charAt(ii))){
-            while (outputArr.length && s.charAt(ii) < outputArr[outputArr.length-1] && ii < lastIdx[outputArr[outputArr.length-1]]){
+        if (!chars.has(s[ii])){
+            while (outputArr.length && s[ii] < outputArr[outputArr.length-1] && ii < lastIdx[outputArr[outputArr.length-1]]){
                 chars.delete(outputArr.pop());
             }
 
-            chars.add(s.charAt(ii));
-            outputArr.push(s.charAt(ii));
+            chars.add(s[ii]);
+            outputArr.push(s[ii]);
         }
     }
     return outputArr.join("");
