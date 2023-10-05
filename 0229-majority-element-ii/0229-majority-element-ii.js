8f37893b3bb1fda71359ed39995b32ae2f804728
David Tan <dtannyc1@gmail.com>
2023-10-05 12:50:23 -0400

    Time: 54 ms (82.06%), Space: 44.4 MB (43.33%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,21 +3,57 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    let output = new Set();
-    let dict = ({});
-    let minCount = nums.length/3;
+    // let output = [];
+    // let dict = ({});
+    // let minCount = nums.length/3;
+
+    // for (let num of nums){
+    //     if (!dict[num]){
+    //         dict[num] = [1, false];
+    //     } else {
+    //         dict[num][0]++;
+    //     }
+
+    //     if (dict[num][0] > minCount && !dict[num][1]){
+    //         output.push(num);
+    //         dict[num][1] = true;
+    //     }
+    // }
+
+    // return output;
+
+    // Solution 2: output cant be more than 2 numbers!
+    let sol1 = 0, sol2 = 1, count1 = 0, count2 = 0;
 
     for (let num of nums){
-        if (!dict[num]){
-            dict[num] = 1;
+        if (num === sol1){
+            count1++;
+        } else if (num === sol2){
+            count2++;
+        } else if (count1 === 0){
+            count1 = 1;
+            sol1 = num;
+        } else if (count2 === 0){
+            count2 = 1;
+            sol2 = num;
         } else {
-            dict[num]++;
-        }
-
-        if (dict[num] > minCount){
-            output.add(num)
+            count1--;
+            count2--;
         }
     }
 
-    return Array.from(output);
+    // second pass to get an accurate count
+    let output = [];
+    count1 = 0;
+    count2 = 0;
+    for (let num of nums){
+        if (num === sol1){
+            count1++;
+        } else if (num === sol2){
+            count2++;
+        }
+    }
+    if (count1 > nums.length / 3) output.push(sol1);
+    if (count2 > nums.length / 3) output.push(sol2);
+    return output;
 };
