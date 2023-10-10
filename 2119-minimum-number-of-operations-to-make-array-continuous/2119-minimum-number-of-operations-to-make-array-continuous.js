badfebc4ca487a1c51d100a08062f506d43cfdea
David Tan <dtannyc1@gmail.com>
2023-10-10 17:22:59 -0400

    Time: 244 ms (22.22%), Space: 75.1 MB (55.56%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.js
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.js
@@ -19,38 +19,38 @@ var minOperations = function(nums) {
         return 0;
     });
 
-    // let findIdxOf = function(target, left, right){
-    //     if (uniq[left+1] && uniq[left+1] > target) return left;
-    //     if (uniq[right] < target) return right;
+    let findIdxOf = function(target, left, right){
+        if (left + 1 < uniq.length && uniq[left+1] > target) return left;
+        if (uniq[right] < target) return right;
 
-    //     while (left < right) {
-    //         let midIdx = Math.floor((right + left)/2);
-    //         if (uniq[midIdx] === target) {
-    //             return midIdx;
-    //         }
-    //         else if (uniq[midIdx] < target){
-    //             left = midIdx+1;
-    //         } else {
-    //             right = midIdx-1;
-    //         }
-    //     }
-    //     if (uniq[left] > target) return left-1;
-    //     return left;
-    // };
+        while (left < right) {
+            let midIdx = Math.floor((right + left)/2);
+            if (uniq[midIdx] === target) {
+                return midIdx;
+            }
+            else if (uniq[midIdx] < target){
+                left = midIdx+1;
+            } else {
+                right = midIdx-1;
+            }
+        }
+        if (uniq[left] > target) return left-1;
+        return left;
+    };
 
     //console.log(uniq)
     let maxLen = 1;
 
     // let output = findIdxOf(uniq[0] + nums.length - 1, 0, uniq.length-1);
     // [1,10,100,1000], target = 100 + 4 - 1 = 103, 2, 3
-    let right = 0;
+    let output = 0;
     for (let ii = 0; ii < uniq.length; ii++){ // [1,2,3,4] => for 1, search for 4
         // find where target = uniq[ii] + nums.length - 1 is via binary search
-        // output = findIdxOf(uniq[0] + nums.length - 1, Math.max(output, ii), uniq.length-1);
-        while (uniq[right + 1] <= uniq[ii] + nums.length - 1 && right < uniq.length){
-            right++;
-        }
-        let max = right - ii + 1;
+        output = findIdxOf(uniq[ii] + nums.length - 1, Math.max(output, ii), uniq.length-1);
+        // while (uniq[right + 1] <= uniq[ii] + nums.length - 1 && right < uniq.length){
+        //     right++;
+        // }
+        let max = output - ii + 1;
         //console.log(output, max)
         if (max > maxLen){
             maxLen = max;
@@ -61,4 +61,32 @@ var minOperations = function(nums) {
     }
     
     return nums.length - maxLen;
+
+
+    // // Sliding Window:
+    // let set = new Set(nums);
+    // let uniq = Array.from(set);
+
+    // uniq.sort((a,b) => {
+    //     if (a < b) return -1;
+    //     if (a > b) return 1;
+    //     return 0;
+    // });
+
+    // let maxLen = 1;
+    // let right = 0;
+    // for (let ii = 0; ii < uniq.length; ii++){ 
+    //     while (uniq[right + 1] <= uniq[ii] + nums.length - 1 && right < uniq.length){
+    //         right++;
+    //     }
+    //     let max = right - ii + 1;
+    //     if (max > maxLen){
+    //         maxLen = max;
+    //     }
+    //     if (maxLen > uniq.length - ii){
+    //         break;
+    //     }
+    // }
+    
+    // return nums.length - maxLen;
 };
