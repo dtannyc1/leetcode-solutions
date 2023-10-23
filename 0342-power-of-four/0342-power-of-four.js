4d9c0215ce99e78aeca47acf3c02c9acc97d0d8e
David Tan <dtannyc1@gmail.com>
2023-10-23 10:37:14 -0400

    Time: 67 ms (20.62%), Space: 43.2 MB (74.3%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -3,8 +3,16 @@
  * @return {boolean}
  */
 var isPowerOfFour = function(n) {
+    // recursive solution
+    // if (n <= 0) return false
+    // if (n === 1 || n === 4) return true
+    // if (n % 4) return false
+    // return isPowerOfFour(n/4)
+
+    // use logarithms
     if (n <= 0) return false
     if (n === 1 || n === 4) return true
-    if (n % 4) return false
-    return isPowerOfFour(n/4)
+    let pow = Math.log(n)/Math.log(4);
+    // console.log(pow, Number.isInteger(pow))
+    return Number.isInteger(pow);
 };
