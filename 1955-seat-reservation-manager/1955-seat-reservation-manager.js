bb750199088b1e4f516f936ba6a9955235c4580e
David Tan <dtannyc1@gmail.com>
2023-11-06 11:54:12 -0500

    Time: 3066 ms (12.5%), Space: 114.2 MB (56.25%) - LeetHub

--- 1955-seat-reservation-manager/1955-seat-reservation-manager.js
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.js
@@ -2,14 +2,22 @@
  * @param {number} n
  */
 var SeatManager = function(n) {
-    this.seats = new Array(n).fill(0).map((val,ii) => ii+1).reverse();
+    //this.seats = new Array(n).fill(0).map((val,ii) => ii+1).reverse();
+    this.seats = new Array(n).fill(true);
+    this.lowestSeat = 0;
 };
 
 /**
  * @return {number}
  */
 SeatManager.prototype.reserve = function() {
-    return this.seats.pop();
+    //return this.seats.pop();
+    let output = this.lowestSeat;
+    this.seats[this.lowestSeat] = false;
+    while (this.lowestSeat < this.seats.length && !this.seats[this.lowestSeat]){
+        this.lowestSeat++;
+    }
+    return output+1;
 };
 
 /** 
@@ -17,13 +25,16 @@ SeatManager.prototype.reserve = function() {
  * @return {void}
  */
 SeatManager.prototype.unreserve = function(seatNumber) {
-    this.seats.push(seatNumber);
+    /*this.seats.push(seatNumber);
     let jj = this.seats.length-1;
     while (jj > 0 && this.seats[jj]>this.seats[jj-1]){
         [this.seats[jj-1], this.seats[jj]] = [this.seats[jj], this.seats[jj-1]];
         jj--;
     }
     return null;
+    */
+    this.seats[seatNumber-1] = true;
+    if (this.lowestSeat > seatNumber-1) this.lowestSeat = seatNumber-1;
 };
 
 /** 
