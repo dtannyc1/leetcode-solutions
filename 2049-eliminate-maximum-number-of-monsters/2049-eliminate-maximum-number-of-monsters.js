09cb68f2eeef5b3d2d137ba7a09cf749490396ef
David Tan <dtannyc1@gmail.com>
2023-11-07 11:22:42 -0500

    Time: 114 ms (89.19%), Space: 59.3 MB (37.84%) - LeetHub

--- 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.js
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.js
@@ -6,12 +6,14 @@
 var eliminateMaximum = function(dist, speed) {
     // get a list of times at which the monsters would kill the player
     let endTimes = dist.map((loc,ii) => Math.ceil(loc/speed[ii]));
-    endTimes.sort((a,b) => {
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
-    });
 
+    // // slowest part is nlogn
+    // endTimes.sort((a,b) => {
+    //     if (a < b) return -1;
+    //     if (a > b) return 1;
+    //     return 0;
+    // });
+    /*
     let i;
     for (i = 0; i < endTimes.length; i++){
         if (endTimes[i] < i+1){
@@ -19,5 +21,18 @@ var eliminateMaximum = function(dist, speed) {
         }
     };
     return i;
+    */
 
+    let numMonsters = new Array(dist.length).fill(0);
+    for (let time of endTimes){
+        if (time < numMonsters.length) numMonsters[time]++;
+    };
+
+    let runningCount = 0;
+    let i;
+    for (i = 0; i < numMonsters.length; i++){
+        runningCount += numMonsters[i];
+        if (runningCount > i){ break;}
+    }
+    return i;
 };
