81f3e29035aae84b62d93841d9a411c85224ee0d
David Tan <dtannyc1@gmail.com>
2024-02-01 11:45:54 -0500

    Time: 343 ms (42.86%), Space: 80 MB (94.92%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
@@ -12,9 +12,19 @@ var divideArray = function(nums, k) {
         return 0;
     });
     let output = [];
-    while (sorted.length){
-        if (sorted[2]-sorted[0] <= k){
-            output.push(sorted.splice(0,3));
+    // // solution 1:
+    // while (sorted.length){
+    //     if (sorted[2]-sorted[0] <= k){
+    //         output.push(sorted.splice(0,3));
+    //     } else {
+    //         return [];
+    //     }
+    // }
+    
+    // solution 2: don't mutate sorted array
+    for (let i = 0; i < sorted.length; i = i+3){
+        if (sorted[i+2]-sorted[i] <= k){
+            output.push(sorted.slice(i,i+3));
         } else {
             return [];
         }
